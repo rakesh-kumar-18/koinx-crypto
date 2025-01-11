3e65c93c9ded55b9b5f08d139064b2659aada886
Rakesh Kumar Sahoo <sahoorakeshkumar42@gmail.com>
2025-01-11 17:33:16 +0530

    add controller to express app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import fetchAndStoreCryptoData from "./controllers/crypto.controller.js";
 
 const app = express();
+
+fetchAndStoreCryptoData();
 
 export default app;
